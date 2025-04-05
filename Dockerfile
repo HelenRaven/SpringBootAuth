8b78701454a4d66ede167b0e07bcd351b350ea01
HelenRaven <HelenRaven@mail.ru>
2025-04-05 20:03:14 +0700

    Test containers

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,4 @@
 FROM eclipse-temurin:21-jre-alpine
-
-EXPOSE 8888
-
-COPY target/springBootAuth-0.0.1-SNAPSHOT.jar app.jar
-
-CMD ["java", "-jar", "app.jar"]
+EXPOSE 8881
+ADD target/springBootAuth-0.0.1-SNAPSHOT.jar myapp.jar
+ENTRYPOINT ["java","-jar","/myapp.jar"]
--- src/test/java/org/example/springbootauth/SpringBootAuthApplicationTests.java
+++ src/test/java/org/example/springbootauth/SpringBootAuthApplicationTests.java
@@ -1,9 +1,79 @@
 package org.example.springbootauth;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONArray;
+import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.shaded.org.apache.commons.lang3.StringUtils;
 
-@SpringBootTest
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootAuthApplicationTests {
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    private static final GenericContainer<?> devApp = new GenericContainer<>("devapp")
+            .withExposedPorts(8888);
+    private static final GenericContainer<?> prodApp = new GenericContainer<>("prodapp")
+            .withExposedPorts(8881);
+    private static final String name = "Ann";
+    private static final String password = "pass";
+    private static final String[] authorities = new String[] { "READ" };
+    private static HttpEntity<String> request;
+
+    @BeforeAll
+    public static void setUp() throws JSONException {
+        JSONObject userDetailsRequestJson = new JSONObject();
+        userDetailsRequestJson.put("name", name);
+        userDetailsRequestJson.put("password", password);
+        userDetailsRequestJson.put("authorities", new JSONArray(authorities));
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
+
+        request = new HttpEntity<>(userDetailsRequestJson.toString(), headers);
+
+        devApp.start();
+        prodApp.start();
+    }
+
+    @Test
+    void devAppLoads() throws JSONException {
+        restTemplate.postForEntity("http://localhost:" + devApp.getMappedPort(8888) + "/signIn", request, String.class);
+
+        String baseUrl = "http://localhost:" + devApp.getMappedPort(8888) + "/authorize";
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(baseUrl)
+                .queryParam("name", name)
+                .queryParam("password", password);
+
+        ResponseEntity<String> forEntity = restTemplate.getForEntity(builder.toUriString(), String.class);
+        Assertions.assertEquals("[\"" + StringUtils.join(authorities, ",") + "\"]", forEntity.getBody());
+    }
+
+    @Test
+    void prodAppLoads() {
+        restTemplate.postForEntity("http://localhost:" + prodApp.getMappedPort(8881) + "/signIn", request, String.class);
+
+        String baseUrl = "http://localhost:" + prodApp.getMappedPort(8881) + "/authorize";
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(baseUrl)
+                .queryParam("name", name)
+                .queryParam("password", password);
+
+        ResponseEntity<String> forEntity = restTemplate.getForEntity(builder.toUriString(), String.class);
+        Assertions.assertEquals("[\"" + StringUtils.join(authorities, ",") + "\"]", forEntity.getBody());
+    }
 
 }
